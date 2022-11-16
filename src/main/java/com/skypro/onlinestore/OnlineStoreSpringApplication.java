57d0e0e9152d24d85bb207200b6d498d5dd8edc7
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-11-16 22:05:34 +0400

    first commit.

---
+++ src/main/java/com/skypro/onlinestore/OnlineStoreSpringApplication.java
@@ -0,0 +1,15 @@
+package com.skypro.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreSpringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OnlineStoreSpringApplication.class, args);
+
+    }
+
+}
